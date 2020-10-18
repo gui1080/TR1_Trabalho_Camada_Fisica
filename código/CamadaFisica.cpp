717d243e9786a0b9d8892bad9c796b265465b327
Shudugen <44250965+Shudugen@users.noreply.github.com>
2020-10-18 14:53:54 -0300

    Fluxo de bits

--- código/CamadaFisica.cpp
+++ código/CamadaFisica.cpp
@@ -1,140 +1,173 @@
-
 #include <string>
-using namespace std;
+	using namespace std;
 
-#include <stdexcept>
-#include <iostream>
-#include <bits/stdc++.h> 
-#include <bitset>
-#include <stdlib.h>
+	#include <stdexcept>
+	#include <iostream>
+	#include <bits/stdc++.h>
+	#include <bitset>
+	#include <stdlib.h>
 
+	#include "CamadaFisica.h"
 
-#include "CamadaFisica.h"
+	void converte(string mensagem, int quadro[], int pos){
 
-void converte(string mensagem, int quadro[], int pos){
+	int x, j, i;
 
-  int x, j, i; 
+	bitset<8> set(mensagem[pos]);
 
-  bitset<8> set(mensagem[pos]); 
+	//cout << set << endl;
 
-  cout << set << endl; 
+	j = ((pos*8)-1) + 8;
 
-  j = ((pos*8)-1);
+  //cout << j << endl;
 
-  int novo_quadro[8]; 
+	int novo_quadro[8];
 
-  for(x=0; x<mensagem.size() ; x++){
+//	for(x=0; x<mensagem.size() ; x++){
 
-    for(i = 0; i < 8 ; i++){
+	for(i = 0; i < 8 ; i++){
 
-      if(set.test(i)){
-        
-        //quadro[j] = 1;
+	if(set.test(i)){
 
-        cout << "a" << endl; 
-        j--; 
-      }
-      else{
-        
-        //quadro[j] = 0;
-        
-        j--;
-      }
+	quadro[j] = 1;
 
-    } 
-    //set.reset(); 
+	//cout << "a" << endl;
+	j--;
+	}
+	else{
 
-  }
+	quadro[j] = 0;
 
-    return; 
-  
+	j--;
+	}
 
-}
+	}
+	//set.reset();
 
-
-void CamadaDeAplicacaoTransmissora(string mensagem){
-
-  //int quadro[] = mensagem // trabalhando com bits
-  //chama a proxima camada
-
-  // VAMOS PASSAR COM UM FOR POR CADA LETRA
-  // CADA LETRA = 2 HEXADECIMAIS = 8 BITS
-  // O ARRAY DE INTEIRO VAI SER TAMANHO str.size() * 8, aloca dinamicamente pra ficar legal
-  // usa funcao pronta pra pegar cada letra e traduzir pra hexa, que traduz pra binario (utf-8)
-
-  // cout<<"What is the letter you wanna convert?"<<endl;
-  // cin >> letter;
-  // cout << bitset<8>(letter).to_string() << endl;
-
-  //int const tamanho = 8*(mensagem.size()); 
-
-
-  int i; 
-  int j;
-  int tamanho_quadro_final = 8*(mensagem.size()); 
-  int x; 
-
-  cout << tamanho_quadro_final <<endl; 
-
-  int *quadro, *letra_atual;
-  quadro = new (nothrow) int[tamanho_quadro_final]; 
-
-  converte(mensagem, quadro, 0);
-
-  for(i = 0; i < 8 ; i++){
-
-    cout << quadro[i];  
-
-  }
-
-  //converte(mensagem, quadro, 1);
-  
-  // CamadaFisicaTransmissora(quadro);
-
-} // fim do metodo
-
-void CamadaFisicaTransmissora(int quadro[]){
-  int tipoDeCodificacao = 1;
-  int fluxoBrutoDeBits[10];
-
-  switch (tipoDeCodificacao){
-    case 0: //codificacao binaria
-      
-      //fluxoBrutoDeBits = CamadaFisicaTransmissoraCodificacaoBinaria(quadro);
-      
-      break;
-
-    case 1: //codificacao manchester
-      
-      // fluxoBrutoDeBits = CamadaFisicaTransmissoraCodificacaoManchester(quadro);
-      
-      break;
-
-    case 2: //codificacao manchester diferencial
-      // fluxoBrutoDeBits = CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(quadro);
-      
-      break;
-  
-  } // fim do switch/case
-
-  //MeioDeComunicacao(fluxoBrutoDeBits);
-
-}
+//	}
 
 /*
-int [] CamadaFisicaTransmissoraCodificacaoBinaria(int quadro[]){
-  // implementacao do algoritmo
+  for(i = 0; i < 8 ; i++){
 
-} // fim do metodo CamadaFisicaTransmissoraCodificacaoBinaria
+  cout << quadro[i];
 
-int [] CamadaFisicaTransmissoraCodificacaoManchester(int quadro[]){
-  // implementacao do algoritmo
+  }
 
-} // fim do metodo CamadaFisicaTransmissoraCodificacaoManchester
+  cout << endl;
+  */
+	return;
 
-int [] CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(int quadro[]){
-  // implementacao do algoritmo
 
-} // fim do metodo CamadaFisicaTransmissoraCodificacaoManchesterDiferencial
+	}
 
-*/
+
+	void CamadaDeAplicacaoTransmissora(string mensagem){
+
+	//int quadro[] = mensagem // trabalhando com bits
+	//chama a proxima camada
+
+	// VAMOS PASSAR COM UM FOR POR CADA LETRA
+	// CADA LETRA = 2 HEXADECIMAIS = 8 BITS
+	// O ARRAY DE INTEIRO VAI SER TAMANHO str.size() * 8, aloca dinamicamente pra ficar legal
+	// usa funcao pronta pra pegar cada letra e traduzir pra hexa, que traduz pra binario (utf-8)
+
+	// cout<<"What is the letter you wanna convert?"<<endl;
+	// cin >> letter;
+	// cout << bitset<8>(letter).to_string() << endl;
+
+	//int const tamanho = 8*(mensagem.size());
+
+
+	int i;
+	int j;
+	int tamanho_quadro_final = 8*(mensagem.size());
+	int x;
+
+	cout << tamanho_quadro_final <<endl;
+
+	int *quadro, *letra_atual;
+	quadro = new (nothrow) int[tamanho_quadro_final];
+
+for(j = 0; j < mensagem.size(); j++){
+
+  converte(mensagem, quadro, j);
+
+  cout << mensagem[j] << " se tornou ";
+
+  for(i = j*8; i < (j*8 + 8)  ; i++){
+
+  cout << quadro[i];
+
+  }
+
+  cout << endl;
+}
+
+for(j = 0; j < tamanho_quadro_final; j++){
+
+  cout << quadro[j];
+
+}
+cout << endl;
+/*
+	converte(mensagem, quadro, 1);
+
+	for(i = 0; i < 8 ; i++){
+
+	cout << quadro[i];
+
+	}
+
+  cout << endl;
+  */
+	//converte(mensagem, quadro, 1);
+
+	// CamadaFisicaTransmissora(quadro);
+
+	} // fim do metodo
+
+	void CamadaFisicaTransmissora(int quadro[]){
+	int tipoDeCodificacao = 1;
+	int fluxoBrutoDeBits[10];
+
+	switch (tipoDeCodificacao){
+	case 0: //codificacao binaria
+
+	//fluxoBrutoDeBits = CamadaFisicaTransmissoraCodificacaoBinaria(quadro);
+
+	break;
+
+	case 1: //codificacao manchester
+
+	// fluxoBrutoDeBits = CamadaFisicaTransmissoraCodificacaoManchester(quadro);
+
+	break;
+
+	case 2: //codificacao manchester diferencial
+	// fluxoBrutoDeBits = CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(quadro);
+
+	break;
+
+	} // fim do switch/case
+
+	//MeioDeComunicacao(fluxoBrutoDeBits);
+
+	}
+
+	/*
+	int [] CamadaFisicaTransmissoraCodificacaoBinaria(int quadro[]){
+	// implementacao do algoritmo
+
+	} // fim do metodo CamadaFisicaTransmissoraCodificacaoBinaria
+
+	int [] CamadaFisicaTransmissoraCodificacaoManchester(int quadro[]){
+	// implementacao do algoritmo
+
+	} // fim do metodo CamadaFisicaTransmissoraCodificacaoManchester
+
+	int [] CamadaFisicaTransmissoraCodificacaoManchesterDiferencial(int quadro[]){
+	// implementacao do algoritmo
+
+	} // fim do metodo CamadaFisicaTransmissoraCodificacaoManchesterDiferencial
+
+	*/
